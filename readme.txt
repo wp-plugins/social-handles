b879a1d9e69ca04c6127684ec4888b44aa80043e
RiverCityGraphix <RiverCityGraphix@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-07 18:42:01 +0000

    Initial Commit
    git-svn-id: https://plugins.svn.wordpress.org/social-handles/trunk@582818 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,42 @@
+=== Social Handles ===
+Contributors: rivercitygraphix
+Donate link: https://www.paypal.com/us/cgi-bin/webscr?cmd=_flow&SESSION=zLGfL_Ci8B0vKfIBWkb6FkuIbLVdAGREmdil5u34JDrfMjIHeDFt38v_58e&dispatch=5885d80a13c0db1f8e263663d3faee8d8494db9703d295b4a2116480ee01a05c
+Tags: social, handle, replace, handles, link, twitter, facebook, youtube, page, account, profile, wordpress, plugin, content, find, @, tag, hashtag, hash, trend, networking,
+Requires at least: 3.0
+Tested up to: 3.4.1
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+The Social Handles plugin allows you to quickly replace social handles with links to their respective profiles.
+
+== Description ==
+
+The Social Handles plugin allows you to quickly replace social handles with links to their respective profiles.
+
+Features:
+
+* Replace handles for Facebook, Twitter, Youtube, Twitter hashtags, and Wordpress.
+* Easy Installation.  Simply install the plugin and your handles will be replaced.
+* Easy shorthand. @username for Twitter. yt@username for YouTube. fb@pagename for Facebook.  wp@username for Wordpress. #hashtag for Twitter Hashtags.
+* Integrates with site's current link styling.
+* Use as many social handles on a page as you like.
+
+== Installation ==
+
+1. Upload `social-handles.php` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+Ask some questions and I will put the answers here.
+
+== Screenshots ==
+
+1. Use the easy shorthand with your social handles in your content.
+2. On the front end, the shorthand converts your handles into links to your respective social profiles.
+
+== Changelog ==
+
+= 1.0 =
+* Plugin Launch
---
+++ social-handles.php
@@ -0,0 +1,47 @@
+<?php
+/*
+Plugin Name: Social Handles
+Plugin URI: http://www.rivercitygraphix.com
+Description: A plugin that allows you to replace social handles with links to their respective profiles.
+Author: Kevin Olson
+Version: 1.0
+Author URI: http://www.rivercitygraphix.com
+*/
+
+add_filter( "the_content", "replace_twitter_handles" );
+add_filter( "the_content", "replace_youtube_handles" );
+add_filter( "the_content", "replace_facebook_page_handles" );
+add_filter( "the_content", "replace_wordpress_handles" );
+add_filter( "the_content", "replace_twitter_hashtags_handles" );
+
+function replace_twitter_handles($content) {
+	$pattern= '/(?<=^|(?<=[^a-zA-Z0-9-_\.]))@([A-Za-z]+[A-Za-z0-9_]+)/i';
+	$replace= '@<a href="http://www.twitter.com/$1">$1</a>';
+	$content= preg_replace($pattern, $replace, $content);
+	return $content;
+}
+function replace_youtube_handles($content) {
+	$pattern= '/(?<=^|(?<=[^a-zA-Z0-9-_\.]))yt@([A-Za-z]+[A-Za-z0-9_]+)/i';
+	$replace= '@<a href="http://www.youtube.com/$1">$1</a>';
+	$content= preg_replace($pattern, $replace, $content);
+	return $content;
+}
+function replace_facebook_page_handles($content) {
+	$pattern= '/(?<=^|(?<=[^a-zA-Z0-9-_\.]))fb@([A-Za-z]+[A-Za-z0-9_]+)/i';
+	$replace= '@<a href="http://www.facebook.com/$1">$1</a>';
+	$content= preg_replace($pattern, $replace, $content);
+	return $content;
+}
+function replace_wordpress_handles($content) {
+	$pattern= '/(?<=^|(?<=[^a-zA-Z0-9-_\.]))wp@([A-Za-z]+[A-Za-z0-9_]+)/i';
+	$replace= '@<a href="http://profiles.wordpress.org/$1">$1</a>';
+	$content= preg_replace($pattern, $replace, $content);
+	return $content;
+}
+function replace_twitter_hashtags_handles($content) {
+	$pattern= '/(?<=^|(?<=[^a-zA-Z0-9-_\.]))#([A-Za-z]+[A-Za-z0-9_]+)/i';
+	$replace= '#<a href="http://www.twitter.com/search/$1">$1</a>';
+	$content= preg_replace($pattern, $replace, $content);
+	return $content;
+}
+?>
